6c6339382e8860e1f8bd653f9d35dafec2e500e2
bairui <bairui@huami.com>
2020-08-14 16:17:14 +0800

    ref |> 01背包动态规划优化一维

--- DP-01Pack.cpp
+++ DP-01Pack.cpp
@@ -8,21 +8,22 @@ int main()
     int w[5] = {0, 2, 3, 4, 5}; //商品的体积2、3、4、5
     int v[5] = {0, 3, 4, 5, 6}; //商品的价值3、4、5、6
     int bagV = 8;               //背包大小
-    int dp[5][9] = {{0}}, a;    //动态规划表
+    int dp[9] = {0}, a;    //动态规划表
 
     for (int i = 1; i <= 4; i++)
-        for (int j = 1; j <= bagV; j++)
-            if (j < w[i])
-                dp[i][j] = dp[i - 1][j];
-            else
-                a = dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-    cout << a << endl;
+        {
+            for (int j = bagV; j >= w[i]; j--)
+                dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+
+            for (int j = 0; j < 9; j++)
+                cout << dp[j] << "    ";
+            cout << endl;
+        }
+    cout << dp[bagV] << endl;
     //动态规划表的输出
-    for (int i = 0; i < 5; i++)
-    {
+    //for (int i = 0; i < 5; i++)
         for (int j = 0; j < 9; j++)
-            cout << dp[i][j] << ' ';
+            cout << dp[j] << ' ';
         cout << endl;
-    }
     return 0;
 }
